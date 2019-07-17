1e8fc37bf7741310029d53ccf6523b663e08631e
Alexander Kudryavtsev <alexander.kdv@gmail.com>
2019-07-17 10:39:41 +0200

    Add a case with multiple meanings in one entry

--- matchGuideWord.js
+++ matchGuideWord.js
@@ -1,7 +1,7 @@
 
 module.exports = function matchGuideWord (meaning) {
-  const match = /(\\"t?o?\s?)(\D+?)(,|;)/.exec(meaning)
+  const regExp = new RegExp('(\\\\"t?o?\\s?)(\\D+?)(,|;)')
+  const match = regExp.exec(meaning)
   return match[2]
-
 }
 
--- matchGuideWord.test.js
+++ matchGuideWord.test.js
@@ -4,7 +4,8 @@ test.each([
   ['"\\\\~ \\"veiled, covered\"', 'veiled'],
   ['\\"rare; valuable', 'rare'],
   ['OA, OB, jB \\"to be big, to be great, be strong (Am.)', 'be big'],
-  ['"(*i/i*; stat. *(w)aruq*, occas. *uruq*) \\\\[SIG7\\\\] \\"become green, yellow; pale\\" of face, invalid; of fluids, fruit, glass, star"', 'become green']
+  ['"(*i/i*; stat. *(w)aruq*, occas. *uruq*) \\\\[SIG7\\\\] \\"become green, yellow; pale\\" of face, invalid; of fluids, fruit, glass, star"', 'become green'],
+  ['\\"make manifest, appear\\", \\"produce, create\\"; \\"make clear, display\\"; \\"make glorious\\" kingship, name, deeds etc.', 'make manifest']
 ])('%s', (meaning, guideWord) => {
   const strippedMeaning = matchGuideWord(meaning)
   expect(strippedMeaning).toEqual(guideWord)
