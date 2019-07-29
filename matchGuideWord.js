644a926146e0fb55359e10357ef7970bbd49c203
Jussi Laasonen <jussi.laasonen@lmu.de>
2019-07-29 17:33:55 +0200

    Simplify matchGuideWord.js

--- matchGuideWord.js
+++ matchGuideWord.js
@@ -1,49 +1,33 @@
-const pos = require('pos')
+const { Lexer, Tagger } = require('pos')
 
 const defaultGuideWord = ''
 
 function checkForGeneralisedMeaning (meaning) {
-  const regExp = /^\(\D+\)/
-  const match = regExp.exec(meaning)
-  return match !== null ? match[0] : defaultGuideWord
+  const match = /^\(\D+\)/.exec(meaning)
+  return match ? match[0] : defaultGuideWord
 }
 
 function checkForVerb (meaning) {
-  const regExp = /(")(to\s)(\D+?)("|,|;|\s[A-Z])/
-  const match = regExp.exec(meaning)
+  const match = /(")(to\s)(\D+?)("|,|;|\s[A-Z])/.exec(meaning)
   if (match) {
-    const tagger = new pos.Tagger()
-    const words = new pos.Lexer().lex(match[3])
-    const tagged = tagger.tag(words)
-    return tagged[0][1] === 'VB' ? match[3] : `to ${match[3]}`
+    const firstMeaning = match[3]
+    const words = new Lexer().lex(firstMeaning)
+    const [[, tag]] = new Tagger().tag(words)
+    return tag === 'VB' ? firstMeaning : `to ${firstMeaning}`
   } else {
     return defaultGuideWord
   }
 }
 
 function checkForUnknownMeaning (meaning) {
-  const regExp = new RegExp('mng. unkn.')
-  const match = regExp.exec(meaning)
-  if (match !== null) {
-    const replacedMatch = match.join('').replace(/mng. unkn./g, 'meaning unknown')
-    return replacedMatch
-  } else {
-    return defaultGuideWord
-  }
+  return /mng\. unkn\./.test(meaning)
+    ? 'meaning unknown'
+    : defaultGuideWord
 }
 
 module.exports = function matchGuideWord (meaning) {
-  const regExp = /(")(\D+?)("|,|;|\s[A-Z])/
-  const match = regExp.exec(meaning)
-  if (match !== null) {
-    return checkForVerb(match[0]) !== defaultGuideWord
-      ? checkForVerb(meaning)
-      : match[2]
-  } else {
-    return checkForUnknownMeaning(meaning) !== defaultGuideWord
-      ? checkForUnknownMeaning(meaning)
-      : checkForGeneralisedMeaning(meaning) !== defaultGuideWord
-        ? checkForGeneralisedMeaning(meaning)
-        : defaultGuideWord
-  }
+  const match = /(")(\D+?)("|,|;|\s[A-Z])/.exec(meaning)
+  return match
+    ? checkForVerb(match[0]) || match[2]
+    : checkForUnknownMeaning(meaning) || checkForGeneralisedMeaning(meaning)
 }
